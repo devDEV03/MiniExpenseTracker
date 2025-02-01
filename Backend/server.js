bfe54282c24ed62e1a7b88d50a2518e180a0ecac
Dev Singhal <123757089+devDEV03@users.noreply.github.com>
2025-02-01 22:25:53 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -12,11 +12,19 @@ const port = process.env.PORT || 5000;
 const corsOptions = {
     origin: 'https://expensetracker-slwo.onrender.com',
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
 };
+
+app.use((req, res, next) => {
+    console.log("Incoming Request Headers:", req.headers);
+    next();
+});
 
 app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
+app.options('*', cors(corsOptions)); 
 app.use("/api/user",require("./routes/userRoutes"));
 app.use("/api/expense",require("./routes/expenseRoutes"));
 
